ae74a226bb4e0cea39a07ca84003f98ea229a00c
Alexsey Loginov <aleniklog@gmail.com>
2022-07-24 23:35:53 +0300

    1st home work

---
+++ SwiftUI-vk/SwiftUI_vkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_vkApp.swift
+//  SwiftUI-vk
+//
+//  Created by Алексей Логинов on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_vkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
